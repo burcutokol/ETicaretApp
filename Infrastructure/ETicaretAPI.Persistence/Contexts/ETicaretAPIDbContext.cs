dad3588d9f5459ef06f603d98b59d82039f23cc4
burcu <tokolburcu@gmail.com>
2025-06-19 23:01:04 +0300

    Interceptor logic was added to update create and update dates.

--- Infrastructure/ETicaretAPI.Persistence/Contexts/ETicaretAPIDbContext.cs
+++ Infrastructure/ETicaretAPI.Persistence/Contexts/ETicaretAPIDbContext.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ETicaretAPI.Domain.Entities;
+using ETicaretAPI.Domain.Entities.Common;
 using Microsoft.EntityFrameworkCore;
 
 namespace ETicaretAPI.Persistence.Contexts
@@ -16,5 +17,34 @@ namespace ETicaretAPI.Persistence.Contexts
         public DbSet<Product> Products { get; set; }
         public DbSet<Order> Orders { get; set; }
         public DbSet<Customer> Customers { get; set; }
+        public override Task<int> SaveChangesAsync( CancellationToken cancellationToken = default) //Interceptor
+        { //This method will be used common changes on entities.
+            var data = ChangeTracker
+                .Entries<BaseEntity>(); //It catches changes on entities or newly created entities
+            foreach (var d in data)
+            {
+                //_ = d.State switch // _ discard => We can use no need to assign result to some field
+                //{ // It reduces memory usage and increase code readability
+                //    case EntityState.Modified
+                //        d.Entity.UpdatedDate = DateTime.UtcNow,
+                //    EntityState.Added => d.Entity.CreatedDate = DateTime.UtcNow,    
+                //};
+
+                switch (d.State)
+                {
+                    case EntityState.Modified:
+                        d.Entity.UpdatedDate = DateTime.UtcNow;
+                        break;
+                    case EntityState.Added:
+                        d.Entity.CreatedDate = DateTime.UtcNow;
+                        break;
+                        
+                }
+
+
+            }
+            
+            return base.SaveChangesAsync(cancellationToken);
+        }
     }
 }
--- Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
+++ Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
@@ -9,43 +9,25 @@ namespace ETicaretAPI.API.Controllers
     {
         readonly private IProductWriteRepository _productWriteRepository;
         readonly private IProductReadRepository _productReadRepository;
+        readonly private IOrderWriteRepository _orderWriteRepository;
+        readonly private ICustomerWriteRepository _customerWriteRepository;
+        readonly private IOrderReadRepository _orderReadRepository;
 
-        public ProductsController(IProductReadRepository productReadRepository, IProductWriteRepository productWriteRepository)
+        public ProductsController(IProductReadRepository productReadRepository, IProductWriteRepository productWriteRepository, IOrderWriteRepository orderWriteRepository, ICustomerWriteRepository customerWriteRepository, IOrderReadRepository orderReadRepository)
         {
             _productReadRepository = productReadRepository;
             _productWriteRepository = productWriteRepository;
+            _orderWriteRepository = orderWriteRepository;
+            _customerWriteRepository = customerWriteRepository;
+            _orderReadRepository = orderReadRepository;
         }
         [HttpGet]
         public async Task Get()
         {
-            await _productWriteRepository.AddRangeAsync(new()
-            {
-                new()
-                {
-                    Id = Guid.NewGuid(),
-                    Name = "Product1",
-                    Price = 100,
-                    CreatedDate = DateTime.UtcNow,
-                    Stock = 10,
-                },
-                new()
-                {
-                    Id = Guid.NewGuid(),
-                    Name = "Product2",
-                    Price = 200,
-                    CreatedDate = DateTime.UtcNow,
-                    Stock = 20,
-                },
-                new()
-                {
-                    Id = Guid.NewGuid(),
-                    Name = "Product3",
-                    Price = 300,
-                    CreatedDate = DateTime.UtcNow,
-                    Stock = 30,
-                }
-                });
-            await _productWriteRepository.SaveAsync();
+            var order = await _orderReadRepository.GetByIdAsync("019789c1-bd30-7772-9f43-764e4f31e307");
+            order.Description = "Updated test";
+            _orderWriteRepository.Update(order);
+            await _orderWriteRepository.SaveAsync();
 
         }
     }
